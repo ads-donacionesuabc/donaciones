99dfe5396c82f1bdeacfe7bfa04ba115fda2eb47
monci07 <davidmoncivais@live.com.mx>
2019-05-21 10:58:03 -0700

    Nuevo

---
+++ DOnaciones/app/src/main/java/com/example/donaciones/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.donaciones;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.support.annotation.NonNull;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_Donaciones:
+                    mTextMessage.setText("Donaciones");
+                    return true;
+                case R.id.navigation_Buscar:
+                    mTextMessage.setText("Buscar");
+                    return true;
+                case R.id.navigation_Cuenta:
+                    mTextMessage.setText("Cuenta");
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        mTextMessage = findViewById(R.id.message);
+        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        navView.setBackgroundColor(Color.parseColor("#009933"));
+    }
+
+}
